a940792bbe5a7b1290ab120a7f9c8636280cf09d
MogsFriend <antares@ffxiv.me>
2021-06-05 03:00:51 +0900

    update for PR

--- OverlayPlugin.Updater/CefInstaller.cs
+++ OverlayPlugin.Updater/CefInstaller.cs
@@ -12,7 +12,7 @@ namespace RainbowMage.OverlayPlugin.Updater
 {
     public class CefInstaller
     {
-        const string CEF_DL = "https://github.com/MogsFriend/OverlayPlugin/releases/download/v0.7.0/CefSharp-{CEF_VERSION}-{ARCH}.DO_NOT_DOWNLOAD";
+        const string CEF_DL = "https://github.com/ngld/OverlayPlugin/releases/download/v0.7.0/CefSharp-{CEF_VERSION}-{ARCH}.DO_NOT_DOWNLOAD";
         const string CEF_VERSION = "90.6.70";
 
         public static string GetUrl()
