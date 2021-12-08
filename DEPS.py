9d7bf627bf13525cd1ccaf32f19f3d6c41815baf
ShadyWhite <44015352+ShadyWhite@users.noreply.github.com>
2021-12-08 12:12:57 +0800

    Update dependencies

--- DEPS.py
+++ DEPS.py
@@ -4,16 +4,16 @@
 
 deps = {
     'ACT': {
-        'url': 'https://github.com/EQAditu/AdvancedCombatTracker/releases/download/3.4.7.268/ACTv3.zip',
+        'url': 'https://github.com/EQAditu/AdvancedCombatTracker/releases/download/3.5.0.273/ACTv3.zip',
         'dest': 'Thirdparty/ACT',
         'strip': 0,
-        'hash': ['sha256', 'adf13a38d0938ce90f8e674f8365b227d933b91636ddf72b26c85702f6e3b808'],
+        'hash': ['sha256', 'b69281760ae3ceb33d8b45a299c9ed5c90668544664b9ade373f548dbb0e86d6'],
     },
     'FFXIV_ACT_Plugin': {
-        'url': 'https://github.com/ravahn/FFXIV_ACT_Plugin/raw/master/Releases/FFXIV_ACT_Plugin_SDK_2.0.6.1.zip',
+        'url': 'https://github.com/ravahn/FFXIV_ACT_Plugin/raw/master/Releases/FFXIV_ACT_Plugin_SDK_2.6.2.3.zip',
         'dest': 'Thirdparty/FFXIV_ACT_Plugin',
         'strip': 0,
-        'hash': ['sha256', '55eed8660848273b285e2f128a65e6822f086c885942be69691baf5c530a7cd6'],
+        'hash': ['sha256', 'bb296dc4d5bd68a369a6dce7078a15d2d0199be6df3fc658b34ade501f9b465c'],
     },
     'curl': {
         'url': 'https://curl.haxx.se/download/curl-7.70.0.tar.xz',
